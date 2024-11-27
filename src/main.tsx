edc65a45569863e4a3d20f3925c53dc001a1996b
davittrend <davidtrendovski@gmail.com>
2024-11-27 20:27:17 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </Provider>
